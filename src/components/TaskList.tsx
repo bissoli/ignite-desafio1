0f0c9f856b0c25ed7179aa75a58ad8c14066342b
Alison Bissoli <bissolidias@gmail.com>
2022-01-02 22:22:05 -0300

    :tada: Challange Finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useState } from "react"
 
-import '../styles/tasklist.scss'
+import "../styles/tasklist.scss"
 
-import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { FiTrash, FiCheckSquare } from "react-icons/fi"
 
 interface Task {
   id: number;
@@ -16,14 +16,36 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle.trim() === "") 
+      return;
+    setTasks([
+      ...tasks,
+      {
+        id: Math.random() * (1000),
+        title: newTaskTitle,
+        isComplete: false
+      },
+    ]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks([
+      ...tasks.map((task:Task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            isComplete: !task.isComplete,
+          };
+        }
+        return task;
+      })
+    ]);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks([...tasks.filter((task:Task) => task.id !== id)]);
   }
 
   return (
